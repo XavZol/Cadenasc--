c7e5d534f8b6e729ebdfb286af986a37a987f7c9
XavZol <alansolis707@gmail.com>
2025-08-05 14:21:29 -0600

    Invertir una cadena con strrev

--- Cadena.cpp
+++ Cadena.cpp
@@ -3,13 +3,11 @@
 #include<cstring>
 using namespace std;
 int main(){
-// Crear una cadena que tenga la siguiente frse "Hola que tal", luego crear otra cadena para preguntarle al usuario su nombre, por ultimo añadir el nombre al final de la primera cadena y mostrar el mensaje completo "Hola que tal, [nombre del usuario]".
-char cad[] = "Hola que tal\n";
-char cad2[50];
-cout<<"Ingrese su nombre:";
-cin.getline(cad2,50,'\n');
-start(cad,cad2);
-cout<<"Saludo completo:\n"<<cad;
+// Invertir una cadena - Funcion en strrev().
+char cad[] = "La guitarra de lolo\n";
+strrev(cad);
+cout<<"Cadena invertida:\n"<<cad;
+// Longitud de una cadena de caracteres. Funcion en strlen().
 getch();
     return 0;
 }
