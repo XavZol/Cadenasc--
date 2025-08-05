1e6b483dda2d5247b57a9862b72eebb1af33d51a
XavZol <alansolis707@gmail.com>
2025-08-05 16:23:58 -0600

    Convertir en dos cadenas de caracteres minusculas  a mayusculas

--- Cadena.cpp
+++ Cadena.cpp
@@ -3,12 +3,23 @@
 #include<cstring>
 using namespace std;
 int main(){
-// Pasar una palabra a Mayusculas - Función strupr()
+// Convertir en dos cadenas de caracteres minusculas a mayusculas, compararlas , y decir si son iguales o no.
+char pala1[20], pala2[20];
 
-char palabra[] ="palabra en minusculas";
+cout<<"Digite la primera palabra:\n";
+cin.getline(pala1,20,'\n');
+cout<<"Digite la segunda palabra:\n";
+cin.getline(pala2,20,'\n');
 
-strupr(palabra);
-cout<<palabra;
+// Convertir a mayusculas ambas palabras
+strupr(pala1);
+strupr(pala2);
+
+if(strcmp(pala1, pala2) ==0){
+    cout<<"Las palabras son iguales";
+} else{
+    cout<<"Las palabras son diferentes";
+}
 
 getch();
  return 0;
