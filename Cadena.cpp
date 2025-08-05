bb7e36710e7101bcdf768daced057d6ada60ddcb
XavZol <alansolis707@gmail.com>
2025-08-05 13:33:07 -0600

    Pedir al usuario ingrese 2 cadenas de caracteres con if

--- Cadena.cpp
+++ Cadena.cpp
@@ -3,12 +3,19 @@
 #include<cstring>
 using namespace std;
 int main(){
-// Comparar cadenas - Funcion strcmp()
-char cadena[]= "Hola\n";
-char cadena2[]= "Hola\n";
-// Hola y Hola son iguales
-if(strcmp(cadena,cadena2) ==0){
-    cout<<"Ambas cadenas son iguales\n";
+// Pedir al usuario que ingrese 2 cadenas de caracteres, e indicar si 
+// ambas cadenas son iguales, en caso de no serlo, indicar cual es mayor alfabeticamente.
+char palabra[30], palabra2[30];
+cout<<"Digite la primera cadena:\n";
+cin.getline(palabra,30,'\n');
+cout<<"Digite la segunda cadena:\n";
+cin.getline(palabra2,30,'\n');
+if(strcmp(palabra, palabra2)==0){
+    cout<<">Ambas palabras son iguales\n";
+} else if (strcmp(palabra, palabra2) > 0){
+    cout<<palabra<<"es mayor alfabeticamente que\n";
+} else{
+    cout<<palabra2<<"es mayor alfabeticamente que\n";
 }
 getch();
     return 0;
