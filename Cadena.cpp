3b9c57cf67e984e862b5494780a80661073eb0c4
XavZol <alansolis707@gmail.com>
2025-08-05 12:58:06 -0600

    Comparar cadenas con funcion strcmp

--- Cadena.cpp
+++ Cadena.cpp
@@ -3,12 +3,13 @@
 #include<cstring>
 using namespace std;
 int main(){
-// Pedir al usuario una de caracteres almacenarla en un arreglo y copia su contenido hacia otro arreglo de caracteres.
-char frase[20], frase2[20];
-cout<<"Digite una frase:\n";
-cin.getline(frase,20,'\n');
-strcpy(frase2,frase); // Copia el contenido de frase a frase2
-cout<<"La frase copiada es:\n"<<frase2<<endl;
+// Comparar cadenas - Funcion strcmp()
+char cadena[]= "Hola\n";
+char cadena2[]= "Hola\n";
+// Hola y Hola son iguales
+if(strcmp(cadena,cadena2) ==0){
+    cout<<"Ambas cadenas son iguales\n";
+}
 getch();
     return 0;
 }
