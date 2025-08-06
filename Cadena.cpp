57498ae7a2bb428dd531f397f79a06e78a0538d1
XavZol <alansolis707@gmail.com>
2025-08-05 21:48:34 -0600

    Pedir su nombre al usuario en Mayuscula con strncmp strlwr

--- Cadena.cpp
+++ Cadena.cpp
@@ -1,15 +1,23 @@
 #include<iostream>
 #include<conio.h>
-#include<cstring>
+#include<string.h>
 using namespace std;
 int main(){
-// Pasar una palabra a minusculas. Función strlwr().
+// Pedir su nombre al usuario en Mayuscula, si su nombre comienza por la letra convertir su nombre a minusculas, caso contrario no convertirlo.
+char nombre[30];
 
-char palabra[] = "FUNDICION DE METALES";
+cout<<"Digite su nombre en mayusculas:\n";
+cin.getline(nombre, 30,'\n');
 
-strlwr(palabra);
-
-cout<<palabra<<endl;
+if(strncmp(nombre, "A", 1)==0){
+    strlwr(nombre);
+    cout<<"Su nombre en minusculas es:\n";
+    cout<<nombre<<endl;
+}
+else{
+    cout<<"El nombre no comienza con la letra A:\n"; 
+    cout<<nombre<<endl;
+}
 
 getch();
  return 0;
